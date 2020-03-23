fc3f50e414dc0057fcde0db7b44ba02636f79d4d
louis baptiste <louisvis@ymail.com>
2020-03-23 21:26:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ function Scooter(year,color,model){
   this.color= color
   this.model= model
 
-  
+
+}
+
+function Driver(name,age,experience) {
+  this.name = year
+  this.age= color
+  this.experience= model
+
 }
